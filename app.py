6e3d16c28c3cf0c50f76bb229457415e7b3f1d09
SamBardwell <s.bardwell@outlook.com>
2023-04-26 15:25:30 -0500

    finished web part

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import (
+    abort, Flask, jsonify, redirect, render_template, request,
+    session, url_for
+)
+app = Flask(__name__)
+import subprocess
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return redirect(url_for('fortune'))
+
+@app.route('/')
+def index():
+    return redirect(url_for('fortune'))
+
+@app.route('/fortune/')
+def fortune():
+    process = subprocess.run(
+                            ['fortune'],
+                            stdout=subprocess.PIPE,
+                            universal_newlines=True
+                            )
+    return '<pre>' + process.stdout + '</pre>'
+
+@app.route('/cowsay/<message>/')
+def cowsay(message):
+    process = subprocess.run(
+                            ['cowsay', message],
+                            stdout=subprocess.PIPE,
+                            universal_newlines=True
+                            )
+    return '<pre>' + process.stdout + '</pre>'
+
+@app.route('/cowfortune/')
+def cowfortune():
+    process1 = subprocess.run(
+                            ['fortune'],
+                            stdout=subprocess.PIPE,
+                            universal_newlines=True
+                            )
+    process = subprocess.run(
+                            f'cowsay {process1.stdout}',
+                            stdout=subprocess.PIPE,
+                            shell=True,
+                            universal_newlines=True
+                            )
+    
+    return '<pre>' + process.stdout + '</pre>'
