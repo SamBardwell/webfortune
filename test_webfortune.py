262a66aea6a18f12633eb1b05a7013e0dd17611e
SamBardwell <s.bardwell@outlook.com>
2023-04-26 15:53:29 -0500

    failing on purpose

--- test_webfortune.py
+++ test_webfortune.py
@@ -21,7 +21,7 @@ def test_cowsay(app, client):
     assert res.status_code == 200
     page_output = res.get_data(as_text=True)
     assert message in page_output
-    assert 'oo' in page_output
+    assert '00' in page_output
 
 def test_cowfortune(app, client):
     res = client.get('/cowfortune/')
